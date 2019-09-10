167f2d9d317546a73b296d31eed594871805a8e5
Ruslan Stadnytskiy <vkontakte11580@gmail.com>
2019-09-10 20:45:44 +0300

    commit1

---
+++ js/script.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    function menu() {
+        document.querySelector('.body').classList.toggle('menu-opened');
+    }
+
+    let arr = document.querySelectorAll('.menu-toggle');
+    for (const value of arr) {
+        value.addEventListener('click', menu);
+    }
+
+};
+  $('a[data-target^="anchor"]').bind('click.smoothscroll',function(){
+        let target = $(this).attr("href"),
+            bl_top = $(target).offset().top - 50;
+        $('body,html').animate({scrollTop:bl_top}, 700);
+        return false;
+    });
+
+$('#slider').slick({
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: "<img src='images/prev.png' class='prev' alt='1'>",
+    nextArrow: "<img src='images/prev.png' class='next' alt='2'>",
+    autoplaySpeed: 2500,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 998,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 710,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+});

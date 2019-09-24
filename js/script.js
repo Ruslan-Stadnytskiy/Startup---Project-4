146029e0e50bb88d58296aa52d1aec667f40afff
Ruslan Stadnytskiy <vkontakte11580@gmail.com>
2019-09-24 14:48:29 +0300

    commit-3

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ window.onload = function () {
 };
 $('a[data-target^="anchor"]').bind('click.smoothscroll', function () {
     let target = $(this).attr("href"),
-        bl_top = $(target).offset().top - 50;
+       bl_top = $(target).offset().top - 50;
     $('body,html').animate({scrollTop: bl_top}, 700);
     return false;
 });
